a3803c9b4396c93407895b082d663ac9a7a2ab94
daugsbi <d.augsbi@gmail.com>
2016-02-22 16:38:24 +0100

    Initialize angularJS Webclient

---
+++ webclient/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularWebclientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularWebclientApp
+ */
+angular.module('angularWebclientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
