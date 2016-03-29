c6980f8373caf490ea3de8a6d65d025304f8dc63
Tobias Giess <tobias.giess@gmail.com>
2016-03-29 11:45:28 +0200

    Adjusted main class to create a deployable war file

--- server/WodssServer/src/main/java/ch/fhnw/wodss/WodssServer.java
+++ server/WodssServer/src/main/java/ch/fhnw/wodss/WodssServer.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.multipart.MultipartResolver;
@@ -13,29 +15,36 @@ import org.springframework.web.multipart.support.StandardServletMultipartResolve
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class WodssServer {
+public class WodssServer extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WodssServer.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WodssServer.class);
+	}
+
 	/**
 	 * Enables multipart/form-data over PUT
+	 * 
 	 * @return the multipart resolver.
 	 */
 	@Bean
 	public MultipartResolver multipartResolver() {
-	   return new StandardServletMultipartResolver() {
-	     @Override
-	     public boolean isMultipart(HttpServletRequest request) {
-	        String method = request.getMethod().toLowerCase();
-	        //By default, only POST is allowed. Since this is an 'update' we should accept PUT.
-	        if (!Arrays.asList("put", "post").contains(method)) {
-	           return false;
-	        }
-	        String contentType = request.getContentType();
-	        return (contentType != null &&contentType.toLowerCase().startsWith("multipart/"));
-	     }
-	   };
+		return new StandardServletMultipartResolver() {
+			@Override
+			public boolean isMultipart(HttpServletRequest request) {
+				String method = request.getMethod().toLowerCase();
+				// By default, only POST is allowed. Since this is an 'update'
+				// we should accept PUT.
+				if (!Arrays.asList("put", "post").contains(method)) {
+					return false;
+				}
+				String contentType = request.getContentType();
+				return (contentType != null && contentType.toLowerCase().startsWith("multipart/"));
+			}
+		};
 	}
 }
