c3af77f4683a1ed5c1d6d0c8fd09a6d2b775adba
Tobias Giess <tobias.giess@gmail.com>
2016-05-23 09:00:23 +0200

    Fixed prod base url

--- webclient/app/scripts/config/config.js
+++ webclient/app/scripts/config/config.js
@@ -11,7 +11,7 @@
 
     // Declare the backend, with an ending slash
     this.baseUrl = 'http://localhost:8080/';
-    // this.baseUrl = 'https://server1070.cs.technik.fhnw.ch/wodss5/server/';
+    // this.baseUrl = 'https://www.cs.technik.fhnw.ch/wodss5/server/';
 
     // Represents the logged in user
     var currentUser = {
