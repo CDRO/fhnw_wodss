817929ad8a03fd04a2b71ed201ddd67d8520a198
daugsbi <d.augsbi@gmail.com>
2016-05-23 16:06:47 +0200

    fix

--- webclient/app/scripts/services/AuthService.js
+++ webclient/app/scripts/services/AuthService.js
@@ -36,7 +36,6 @@
      */
     this.logout = function(){
       return service.deleteObject('token', config.getCurrentToken()).then(function(response){
-        config
         config.setCurrentUser({user: null, id: null, token: null, email: null, timeToLive: null});
       }, function(error){
         $log.error("Log out was not possible cause %o", error);
